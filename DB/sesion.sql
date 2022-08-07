eca0948722a074a964f2395ba7be2becb919d77d
rodrigomorales1111 <rodrigolo441@hotmail.com>
2022-08-07 18:12:21 -0500

    DB
    Retoques finales

---
+++ DB/sesion.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-11-2021 a las 20:56:50
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sesion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+	session_start();
+	if(isset($_SESSION['user'])){
+		header('location:home.php');
+	}
+?>
+
+<?php include('header.php'); ?>
+<body>
+<div class="container">
+	<div style="height:50px;">
+	</div>
+    <div class="row" id="loginform">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="glyphicon glyphicon-lock"></span> Inicio de Sesion
+                    	<span class="pull-right"><span class="glyphicon glyphicon-pencil"></span> <a style="text-decoration:none; cursor:pointer; color:white;" id="signup">Registrate</a></span>
+                    </h3>
+                </div>
+            	<div class="panel-body">
+                	<form role="form" id="logform">
+                    	<fieldset>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Nombre de usuario" name="username" id="username" type="text" autofocus>
+                        	</div>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Contraseña" name="password" id="password" type="password">
+                        	</div>
+                        	<button type="button" id="loginbutton" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> <span id="logtext">Iniciar Sesion</span></button>
+                    	</fieldset>
+                	</form>
+            	</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="signupform" style="display:none;">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="glyphicon glyphicon-pencil"></span> Nuevo Usuario
+                    	<span class="pull-right"><span class="glyphicon glyphicon-log-in"></span> <a style="text-decoration:none; cursor:pointer; color:white;" id="login">Iniciar s.</a></span>
+                    </h3>
+                </div>
+            	<div class="panel-body">
+                	<form role="form" id="signform">
+                    	<fieldset>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Nombre de usuario" name="susername" id="susername" type="text" autofocus>
+                        	</div>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Contraseña" name="spassword" id="spassword" type="password">
+                        	</div>
+                        	<button type="button" id="signupbutton" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-check"></span> <span id="signtext"></span></button>
+                    	</fieldset>
+                	</form>
+            	</div>
+            </div>
+        </div>
+    </div>
+
+    <div id="myalert" style="display:none;">
+    	<div class="col-md-4 col-md-offset-4">
+    		<div class="alert alert-info">
+    			<center><span id="alerttext"></span></center>
+    		</div>
+   	 	</div>
+    </div>
+</div>
+<script src="custom.js"></script>
+</body>
+</html>
